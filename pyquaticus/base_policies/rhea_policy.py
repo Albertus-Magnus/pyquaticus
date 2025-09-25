9ad6f3ecb8cfd41c59f7ad148e0bee48bea7d3bd
magnus.amann <magnus.amann@uni-konstanz.de>
2025-09-25 19:58:59 +0200

    why is this print not showing up?

--- pyquaticus/base_policies/rhea_policy.py
+++ pyquaticus/base_policies/rhea_policy.py
@@ -223,6 +223,7 @@ class RHEA_CTF_Agent(BaseAgentPolicy):
         # compute next action should be already implemented by _get_next_action
         action = self.rhea._get_next_action()
         print("RHEA action:", action)
+        return action
         while True:
             continue
         if action is None:
