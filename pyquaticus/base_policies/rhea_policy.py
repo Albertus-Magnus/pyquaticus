9acb731aa6f9dca94e04a30a22987c95d985360b
magnus.amann <magnus.amann@uni-konstanz.de>
2025-09-25 23:07:12 +0200

    added test reward at seccond location too (i forget easily that env is initiated twice now)

--- pyquaticus/base_policies/rhea_policy.py
+++ pyquaticus/base_policies/rhea_policy.py
@@ -8,7 +8,7 @@ from pyquaticus import pyquaticus_v0
 import pyquaticus.base_policies.base_attack as attack_policy
 import pyquaticus.base_policies.base_defend as defend_policy
 from pyquaticus.base_policies.base_policy import BaseAgentPolicy
-from pyquaticus.utils.rewards import example_reward
+from pyquaticus.utils.rewards import example_reward, test_reward_func, simplest_test
 from pyquaticus.base_policies.utils import (dist_rel_bearing_to_local_rect,
                                             get_avoid_vect,
                                             global_rect_to_abs_bearing,
@@ -63,7 +63,7 @@ class RHEA_CTF_Agent(BaseAgentPolicy):
         config_dict["render_agent_ids"] = True
         config_dict["dynamics"] = ["si", "si"]#["si", "si", "si", "si", "si", "si"]
         config_dict["sim_speedup_factor"] = 3
-        temp_env = pyquaticus_v0.PyQuaticusEnv(team_size=1, config_dict=config_dict, reward_config={'agent_0': caps_and_grabs, 'agent_1': caps_and_grabs},render_mode=None) #best idea I've ever had
+        temp_env = pyquaticus_v0.PyQuaticusEnv(team_size=1, config_dict=config_dict, reward_config={ 'agent_0': simplest_test, 'agent_1': test_reward_func },render_mode=None) #best idea I've ever had
         reset_opts = {'normalize_obs': False, 'normalize_state': False}
         obs, info = temp_env.reset(options=reset_opts)
         env =  temp_env #changes everything (super happy about that, but why no self. here?)
