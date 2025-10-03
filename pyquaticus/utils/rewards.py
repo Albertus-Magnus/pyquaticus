5d5f288d84656a4a95cfe9809ee20c3a1bad6895
Albertus_Magnus <magnus.amann@uni-konstanz.de>
2025-10-03 16:46:31 +0200

    changed some parameters

--- pyquaticus/utils/rewards.py
+++ pyquaticus/utils/rewards.py
@@ -195,17 +195,17 @@ def caps_and_grabs(
     tagging_cooldown: float
 ):
     reward = 0.0
-    prev_num_oob = state['agent_oob'][agents.index(agent_id)]
+    prev_num_oob = prev_state['agent_oob'][agents.index(agent_id)]#remove prev_
     num_oob = state['agent_oob'][agents.index(agent_id)]
     if num_oob > prev_num_oob:
         reward += -1.0
     for t in state['grabs']:
-        prev_num_grabs = state['grabs'][t]
+        prev_num_grabs = prev_state['grabs'][t]#remove prev_
         num_grabs = state['grabs'][t]
         if num_grabs > prev_num_grabs:
             reward += 0.25 if t == team else -0.25
 
-        prev_num_caps = state['captures'][t]
+        prev_num_caps = prev_state['captures'][t]#remove prev_
         num_caps = state['captures'][t]
         if num_caps > prev_num_caps:
             reward += 1.0 if t == team else -1.0
