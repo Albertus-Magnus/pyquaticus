256147a289b7610f03eeb445d28a914ffbf0efd6
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-07 00:47:00 +0200

    testing import changes

--- pyquaticus/base_policies/rhealg_policy.py
+++ pyquaticus/base_policies/rhealg_policy.py
@@ -4,6 +4,7 @@ from typing import Union
 import numpy as np
 
 import copy
+import RollingHorizonEvolutionaryAlgorithm
 from pyquaticus import pyquaticus_v0
 import pyquaticus.base_policies.base_attack as attack_policy
 import pyquaticus.base_policies.base_defend as defend_policy
@@ -23,7 +24,7 @@ from pyquaticus.utils.utils import angle180, dist, line_intersection
 from pyquaticus.utils.rewards import caps_and_grabs
 #from RollingHorizonEvolutionaryAlgorithm.RollingHorizonEA.rhea import RollingHorizonEvolutionaryAlgorithm
 #Start of special rhea import
-from rhea import RollingHorizonEvolutionaryAlgorithm #untested as of now, is this import working or do I need the code below?
+from RollingHorizonEvolutionaryAlgorithm.RollingHorizonEA.rhea import RollingHorizonEvolutionaryAlgorithm #untested as of now, is this import working or do I need the code below?
 # try the normal absolute import first; if it fails, add project root and retry
 # try:
 #     from RollingHorizonEvolutionaryAlgorithm.RollingHorizonEA.rhea import RollingHorizonEvolutionaryAlgorithm
