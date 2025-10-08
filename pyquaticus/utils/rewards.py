abf6da121eb404c1211bc9e5674ca267144834f0
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-09 01:48:38 +0200

    testing

--- pyquaticus/utils/rewards.py
+++ pyquaticus/utils/rewards.py
@@ -235,9 +235,9 @@ def test_reward_func(
     # Determine flag homes and total distance between teams' flag homes
     flag_homes = state['flag_home']#numpy.array(state['flag_home'])
     if team == 'BLUE_TEAM':
-        t = 0#1
+        t = 1
     else:
-        t = 1#0
+        t = 0
     team_home = flag_homes[t]#numpy.array(flag_homes[1])
     opp_home = flag_homes[(t + 1) % 2]#numpy.array(flag_homes[0])
     diff = team_home - opp_home
@@ -253,8 +253,8 @@ def test_reward_func(
     target_flag_pos = team_home if has_flag else opp_home
     diff = position - target_flag_pos
     dist_to_flag = numpy.sqrt(np.sum(diff**2))
-    if dist_to_flag <= total_dist_between_flags:
-        reward += 1.0 - (dist_to_flag / total_dist_between_flags)
+    if dist_to_flag <= 2*total_dist_between_flags:
+        reward += 1.0 - (dist_to_flag / 2*total_dist_between_flags)
     else:
         reward += -0.1
 
@@ -272,7 +272,7 @@ def test_reward_func(
             dists = [float(numpy.linalg.norm(position - p)) for p in opp_positions]
             min_dist = min(dists)
             # normalized by the same total distance between flags, capped at 1.0
-            reward += 0.2 * min(1.0, min_dist / total_dist_between_flags)
+            reward += 0.2 * min(1.0, min_dist / 2*total_dist_between_flags)
 
     # Trying capsngrabs reward added here (capturing flags is necessary to add as reward)
     num_grabs = state['grabs'][t]
