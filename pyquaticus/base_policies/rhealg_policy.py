79b08894f21b9fc1ffc70e75f9191c9442213ab3
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-08 22:12:45 +0200

    testing

--- pyquaticus/base_policies/rhealg_policy.py
+++ pyquaticus/base_policies/rhealg_policy.py
@@ -176,7 +176,7 @@ class RHEA_Environment(Environment):
                 #TODO I may need to compute action for every other agent in the game here so the plan always uses correct other agents. Though for the competition it would be unfair to have an agent know what the other agents do... So how do We implement this? - also rhea would be best, but now doing a deadlock...
                 # F*ck... -we move this to rhea TODO        #one is the rhea agent, zero and two are None (too expensive). The rest are rhea estimates of enemies :/
                 obs, reward, term, trunc, info = self.env.step({'agent_0':zero,'agent_1':one, 'agent_2':two, 'agent_3':three, 'agent_4':four, 'agent_5':five}) #TODO we have only one action so far, what do the other agents do in our plan?
-                scores[i] += reward #This is assuming the reward is per step. If the reward is a score that keeps its pos/neg values from previous steps only the last score needs to be remembered/added.
+                scores[i] += reward['agent_1'] #This is assuming the reward is per step. If the reward is a score that keeps its pos/neg values from previous steps only the last score needs to be remembered/added.
             i += 1
         # The final(?)/summed(?) reward of the solution has to be stored in a list and returned:
         return scores #currently a sum of all steps
