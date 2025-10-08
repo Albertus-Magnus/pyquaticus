85431bfd0c69ce40d8d7bc7236e194f5d8f48b58
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-08 23:57:21 +0200

    testing

--- pyquaticus/base_policies/rhealg_policy.py
+++ pyquaticus/base_policies/rhealg_policy.py
@@ -30,7 +30,7 @@ from pyquaticus.base_policies.base_policy import BaseAgentPolicy
 # #End of special rhea import
 
 # RHEA parameters (adjust here globally)
-rollout_actions_length = 20#100
+rollout_actions_length = 10#100
 mutation_probability = 0.3
 num_evals = 8#100
 # End of RHEA parameters
