7896440e96398d788854ba2b2e01153190ef53ae
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-08 23:56:21 +0200

    fixed copy reward function

--- pyquaticus/base_policies/rhealg_policy.py
+++ pyquaticus/base_policies/rhealg_policy.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 import random
 from typing import Any, Union
 import numpy as np
+
+from pyquaticus.utils.rewards import test_reward_func
 # I Copied rhea code into a subfolder of the current folder for easy access in imports
 from .RollingHorizonEvolAlg.environment import Environment
 from .RollingHorizonEvolAlg.rhea import RollingHorizonEvolutionaryAlgorithm
@@ -105,7 +107,7 @@ class RHEA_Environment(Environment):
         config_dict["dynamics"] = ["si", "si", "si", "si", "si", "si"]
         config_dict["sim_speedup_factor"] = 3
 
-        self.env = pyquaticus_v0.PyQuaticusEnv(team_size=3, config_dict=config_dict,render_mode=None)
+        self.env = pyquaticus_v0.PyQuaticusEnv(team_size=3, config_dict=config_dict, reward_config={'agent_1': test_reward_func},render_mode=None)
         # term_g = {'agent_0':False,'agent_1':False,'agent_2':False}
         # truncated_g = {'agent_0':False,'agent_1':False,'agent_2':False}
         # term = term_g
@@ -177,12 +179,7 @@ class RHEA_Environment(Environment):
                 #TODO I may need to compute action for every other agent in the game here so the plan always uses correct other agents. Though for the competition it would be unfair to have an agent know what the other agents do... So how do We implement this? - also rhea would be best, but now doing a deadlock...
                 # F*ck... -we move this to rhea TODO        #one is the rhea agent, zero and two are None (too expensive). The rest are rhea estimates of enemies :/
                 obs, reward, term, trunc, info = self.env.step({'agent_0':zero,'agent_1':one, 'agent_2':two, 'agent_3':three, 'agent_4':four, 'agent_5':five}) #TODO we have only one action so far, what do the other agents do in our plan?
-                print("obs full: ",obs)
-                print("reward full: ",reward)
-                print("term full: ",term)
-                print("term full: ",term)
-                print("trunc full: ",trunc)
-                print("info returned: ",info)
+                print("Reward returned: ",reward['agent_1'])
                 scores[i] += reward['agent_1'] #This is assuming the reward is per step. If the reward is a score that keeps its pos/neg values from previous steps only the last score needs to be remembered/added.
             i += 1
         # The final(?)/summed(?) reward of the solution has to be stored in a list and returned:
