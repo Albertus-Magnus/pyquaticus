471e33f6662b21fa6a1ed7958ac4ad1d48c5e22f
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-08 22:09:48 +0200

    testing

--- pyquaticus/base_policies/rhealg_policy.py
+++ pyquaticus/base_policies/rhealg_policy.py
@@ -162,7 +162,7 @@ class RHEA_Environment(Environment):
                 #state_copy[action[0]] += action[1] probably old code from the example, delete
                 # Provisional solution for testing: Just one random solution per opponent, as in RHGA with low budget...
                 zero = [0.0, 0]
-                one = solutions
+                one = action
                 two = [0.0, 0]
                 #three = np.array([self.get_random_action() for _ in range(rollout_actions_length)])
                 #this is incorrect, is entire solution where only one action is needed...
