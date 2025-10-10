0ad85a802b58f3f645e9f78b734b470611303ea0
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-10 03:33:48 +0200

    testing

--- pyquaticus/base_policies/multi_rhea_policy.py
+++ pyquaticus/base_policies/multi_rhea_policy.py
@@ -159,7 +159,9 @@ class RHEA_Environment(Environment):
             #do I need to do this multiple times for each opponent?:
             solutri = np.array_split(solution, 3)
             print("solutri: ",solutri)
-            for index in range(len(solutri[0]) - 1):
+            #for index in range(len(solutri[0]) - 1):
+            for index in range(len(solutri[0])):
+
                 print("index: ",index)
                 #state_copy[action[0]] += action[1] probably old code from the example, delete
                 # Provisional solution for testing: Just one random solution per opponent, as in RHGA with low budget...
