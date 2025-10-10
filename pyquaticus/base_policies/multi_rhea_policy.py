35e9a00ced64a7c743dd2374d57e00dc14b542d6
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-10 03:35:17 +0200

    testing

--- pyquaticus/base_policies/multi_rhea_policy.py
+++ pyquaticus/base_policies/multi_rhea_policy.py
@@ -159,8 +159,8 @@ class RHEA_Environment(Environment):
             #do I need to do this multiple times for each opponent?:
             solutri = np.array_split(solution, 3)
             #print("solutri: ",solutri)
-            for index in range(len(solutri[0]) - 1):
-            #for index in range(len(solutri[0])):
+            # for index in range(len(solutri[0]) - 1):
+            for index in range(len(solutri[0])):
 
                 print("index: ",index)
                 #state_copy[action[0]] += action[1] probably old code from the example, delete
