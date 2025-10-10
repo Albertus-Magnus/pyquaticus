b246aa2264a63b9e45e8cec56f2617a77ea57e7f
magnus.amann <magnus.amann@uni-konstanz.de>
2025-10-10 03:25:57 +0200

    changed len() use

--- pyquaticus/base_policies/multi_rhea_policy.py
+++ pyquaticus/base_policies/multi_rhea_policy.py
@@ -158,7 +158,7 @@ class RHEA_Environment(Environment):
         for state_copy, solution in zip(state_copies, solutions):
             #do I need to do this multiple times for each opponent?:
             solutri = np.array_split(solution, 3)
-            for index in range(solutri[0].len):
+            for index in range(len(solutri[0])):
                 #state_copy[action[0]] += action[1] probably old code from the example, delete
                 # Provisional solution for testing: Just one random solution per opponent, as in RHGA with low budget...
                 zero = solutri[0][i] #Should be ith action in the first agents solution block
