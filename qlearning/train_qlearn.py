54eeeeb678e505e6bec60c7f76d3845b53a39a19
Magnus Amann <magnus.amann@uni-konstanz.de>
2026-04-13 16:15:29 +0200

    changed cpu limit for batch 10

--- qlearning/train_qlearn.py
+++ qlearning/train_qlearn.py
@@ -241,8 +241,8 @@ if __name__ == "__main__":
     num_jobs = len(parametersets)
     counter.value = 0
 
-    #num_workers = 15 #15 was best number for my PC in small tests... (cores is 12)
-    num_workers = max(1, os.cpu_count())#TODO test performance of +5 (12 cores, 17 processes now)
+    num_workers = 20 #15 was best number for my PC in small tests... (cores is 12)
+    #num_workers = max(1, os.cpu_count())#TODO test performance of +5 (12 cores, 17 processes now)
     print(f"Selecting {num_workers} as num_workers.")
 
     with Pool(processes=num_workers) as pool:
