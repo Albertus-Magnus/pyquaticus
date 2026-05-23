336df4838164a4ae0c75607a54b4e4094098bbe0
Albertus_Magnus <magnus.amann@uni-konstanz.de>
2026-05-23 18:43:54 +0200

    synching

--- qlearning/evaluate_q_new.py
+++ qlearning/evaluate_q_new.py
@@ -477,7 +477,7 @@ if __name__ == "__main__":
     # names.append(ParameterSet("single_aggressive26", "hard", 0.01, 0.99, 10.0, False, "parametersearch3", "qtrainlog/batch 5/", i, boolchange=False, nrs=10, ep=1000, teamsize3=True, timelimit=600., ignoreseed=False))
     # names.append(ParameterSet("single_aggressive26", "hard", 0.15, 0.95, 10.0, False, "parametersearch4", "qtrainlog/batch 5/", i, boolchange=False, nrs=10, ep=1000, teamsize3=True, timelimit=600., ignoreseed=False))
     # names.append(ParameterSet("single_aggressive26", "hard", 0.01, 0.95, 10.0, False, "parametersearch5", "qtrainlog/batch 5/", i, boolchange=False, nrs=10, ep=1000, teamsize3=True, timelimit=600., ignoreseed=False))
-    if False: #enable when re-running already generated figures or their score prints
+    if True: #enable when re-running already generated figures or their score prints
         if False: #these are not necessary to run ever again, too small sample size or ran before important updates to the code (not suited for comparability)
             # # batch 6b
             # #load_and_call_helper("shortpara1_sharpturns_single_aggressive26_hard_lrate0.1_discount0.99_initq10.0_1nrs_600ep_no_pre", 1)#_nr0") #alternative way to call the visualization
